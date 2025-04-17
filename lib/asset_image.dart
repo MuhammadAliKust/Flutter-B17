e23f4295acccc8068691e43ec54e14af087dd9e3
Muhammad Ali Nizami <kustalics@gmail.com>
2025-04-17 14:45:48 +0500

    Asset and Network Image Done

---
+++ lib/asset_image.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AssetImageDemo extends StatelessWidget {
+  const AssetImageDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Asset Image Demo"),
+      ),
+      body: Image.asset('assets/images/1.jpg',
+      width: MediaQuery.of(context).size.width,
+
+      fit: BoxFit.cover,),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_b17/asset_image.dart';
+
+import 'network_image.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,67 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.green,
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          leading: Icon(
-            Icons.arrow_back,
-            color: Colors.white,
-            size: 25,
-          ),
-          title: Text(
-            "First Screen",
-            style: TextStyle(
-                color: Colors.white, fontSize: 35, fontWeight: FontWeight.bold),
-          ),
-          centerTitle: true,
-          actions: [
-            Icon(
-              Icons.notifications,
-              color: Colors.white,
-            ),
-            Icon(
-              Icons.shopping_cart,
-              color: Colors.white,
-            ),
-          ],
-        ),
-        body: Column(
-          // crossAxisAlignment: CrossAxisAlignment.start,
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 50),
-              child: Text(
-                "Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publishing, and web development.",
-                style: TextStyle(fontSize: 30),
-              ),
-            ),
-            Text(
-              "Second Text",
-              style: TextStyle(fontSize: 30),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Icon(Icons.thumb_up),
-                Icon(Icons.thumb_up),
-                Icon(Icons.thumb_up),
-                Icon(Icons.thumb_up),
-              ],
-            )
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(
-            Icons.add,
-            color: Colors.white,
-          ),
-          backgroundColor: Colors.blue,
-        ),
-      ),
+      home: AssetImageDemo(),
     );
   }
 }
